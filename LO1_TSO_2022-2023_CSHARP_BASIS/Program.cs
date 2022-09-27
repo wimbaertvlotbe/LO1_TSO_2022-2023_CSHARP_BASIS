a3b0d7768a63063c437210f122325c0612e381f7
wimbaertvlotbe <50639680+wimbaert@users.noreply.github.com>
2022-09-27 09:45:56 +0200

    Add project files.

---
+++ LO1_TSO_2022-2023_CSHARP_BASIS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LO1_TSO_2022_2023_CSHARP_BASIS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //voer de opgave uit die via Teams is verdeeld;
+        }
+    }
+}
